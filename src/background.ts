077793d374cedfa5e8d75550b1ae36eca8b2a282
Alex Riveron <ariveron@osisoft.com>
2019-08-14 17:27:39 -0400

    Initial commit

---
+++ src/background.ts
@@ -0,0 +1,7 @@
+import { tAdd } from './backgroundImport';
+
+const i: number = 5;
+const j: string = 'hello';
+
+console.log(`${j} ${i}`);
+console.log(tAdd(i, 10));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const config = {
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
+};
+
+const backgroundConfig = Object.assign({}, config, {
+  name: 'background',
+  entry: './src/background.ts',
+  output: {
+    filename: 'background.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+});
+
+const contentConfig = Object.assign({}, config, {
+  name: 'content',
+  entry: './src/content.ts',
+  output: {
+    filename: 'content.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+});
+
+module.exports = [backgroundConfig, contentConfig];
